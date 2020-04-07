3e311ac159f9144b1ba5aa5ce14725b1f6d8d9bb
nakano yosuke <naninuneno0411@gmail.com>
2020-04-08 01:06:57 +0900

    任意のパスのファイルのinspector表示

---
+++ Assets/AsmdefMultiEdit/Editor/Scripts/AsmdefMultiEditWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace AsmdefMultiEdit.Editor{
+    public class AsmdefMultiEditWindow : EditorWindow
+    {
+        [MenuItem("Window/Open Asmdef Multiple Edit Window")]
+        public static void Open()
+        {
+            //対象アセットをロード
+            var obj = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>("Assets/AsmdefMultiEdit/Example/Piyo/AsmdefMultiEdit.Example.Piyo.asmdef");
+
+            //存在すればPing
+            if (obj)
+                Selection.objects = new[] { obj };
+            
+            var window = CreateWindow<InspectorWindow>();
+        }
+    }
+}
