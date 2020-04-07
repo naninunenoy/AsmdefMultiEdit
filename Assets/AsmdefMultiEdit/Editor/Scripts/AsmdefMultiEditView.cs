5f4ff9b26fa14273afeb67334cd367d493703e66
nakano yosuke <naninuneno0411@gmail.com>
2020-04-07 22:36:46 +0900

    仮のview

---
+++ Assets/AsmdefMultiEdit/Editor/Scripts/AsmdefMultiEditView.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace AsmdefMultiEdit.Editor
+{
+    internal class AsmdefMultiEditView : AssemblyDefinitionImporterInspector 
+    {
+    }
+}
